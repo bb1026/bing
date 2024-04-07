1e03e029134440c55e2c9f237c47e767984a504a
必应 <38268998+bb1026@users.noreply.github.com>
2024-04-07 17:31:54 +0800

    Update js-100.js

--- js/js-100.js
+++ js/js-100.js
@@ -1,31 +1,39 @@
 this.name = "安装小助手";
 this.widget_ID = "100";
-this.version = "v1.0";
-
+this.version = "v1.1";
 
 // 定义网页的 URL
 const pageURL = "https://bb1026.github.io/bing/js/Master.json";
 
-// 弹出输入框让用户输入脚本ID
-let scriptID = "";
-const inputAlert = new Alert();
-inputAlert.title = "输入脚本ID";
-inputAlert.message = "请输入要查找的脚本ID：";
-inputAlert.addTextField("脚本ID:js-", "");
-inputAlert.addAction("确定");
-inputAlert.addCancelAction("取消");
-const inputResult = await inputAlert.present();
-
-// 如果用户点击了确定，则获取输入的脚本ID
-if (inputResult === 0) {
-  scriptID = inputAlert.textFieldValue(0);
-} else {
-  console.log("用户取消了输入。");
-  return;
-}
-
 // 创建文件管理器
 const fm = FileManager.iCloud();
+
+// 从剪贴板中获取链接
+let clipboardLink = await Pasteboard.pasteString();
+
+if (clipboardLink.includes("js-")) {
+  // 从剪贴板中的 URL 提取脚本 ID
+    let startIndex = clipboardLink.lastIndexOf("js-") + 3;
+    let endIndex = clipboardLink.lastIndexOf(".js");
+    var scriptID = clipboardLink.substring(startIndex, endIndex);
+} else {
+  // 弹出输入框让用户输入脚本 ID
+  const inputAlert = new Alert();
+  inputAlert.title = "输入脚本ID";
+  inputAlert.message = "请输入要查找的脚本ID：";
+  inputAlert.addTextField("脚本ID:js-", "");
+  inputAlert.addAction("确定");
+  inputAlert.addCancelAction("取消");
+  const inputResult = await inputAlert.present();
+
+  // 如果用户点击了确定，则获取输入的脚本 ID
+  if (inputResult === 0) {
+    scriptID = inputAlert.textFieldValue(0);
+  } else {
+    console.log("用户取消了输入。");
+    return;
+  }
+}
 
 // 发起网络请求获取网页内容
 const req = new Request(pageURL);
@@ -79,7 +87,7 @@ if (scriptInfo) {
       const runScript = await successAlert.present();
       if (runScript === 1) {
         Safari.open(
-    `scriptable:///run?scriptName=${encodeURIComponent(scriptName)}`
+          `scriptable:///run?scriptName=${encodeURIComponent(scriptName)}`
         );
       }
       console.log(
@@ -92,7 +100,9 @@ if (scriptInfo) {
       cancelAlert.message = `<${scriptName}>已经存在相同名称的脚本，用户取消安装。`;
       cancelAlert.addAction("确定");
       await cancelAlert.present();
-      console.log(`<${scriptName}>已经存在相同名称的脚本，用户取消安装。`);
+      console.log(
+        `<${scriptName}>已经存在相同名称的脚本，用户取消安装。`
+      );
       return;
     }
   } else {
@@ -103,7 +113,10 @@ if (scriptInfo) {
     console.log("[+] 脚本下载完成...");
 
     // 将脚本保存到 Scriptable 的脚本目录中
-    const scriptPath = fm.joinPath(fm.documentsDirectory(), `${scriptName}.js`);
+    const scriptPath = fm.joinPath(
+      fm.documentsDirectory(),
+      `${scriptName}.js`
+    );
     console.log("[#] 开始安装脚本...");
     fm.writeString(scriptPath, scriptContent);
     console.log("[-] 脚本安装完成...");
@@ -117,7 +130,7 @@ if (scriptInfo) {
     const runScript = await successAlert.present();
     if (runScript === 1) {
       Safari.open(
-    `scriptable:///run?scriptName=${encodeURIComponent(scriptName)}`
+        `scriptable:///run?scriptName=${encodeURIComponent(scriptName)}`
       );
     }
     console.log(
