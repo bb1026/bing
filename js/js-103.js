078b93a8493839919a1d516180ee2cebd0b01d44
必应 <38268998+bb1026@users.noreply.github.com>
2024-04-08 12:10:42 +0800

    Update js-103.js

--- js/js-103.js
+++ js/js-103.js
@@ -2,14 +2,16 @@ this.name = "Singapore Bus";
 this.widget_ID = "js-103";
 this.version = "v1.0"; 
 
-let scriptListURL = "https://bb1026.github.io/bing/js/Master.json";
+// 检查更新
+const scriptListURL = "https://bb1026.github.io/bing/js/Master.json";
 let scriptList = await new Request(scriptListURL).loadJSON();
-
+if (scriptList[this.widget_ID]){
 let scriptversion = scriptList[this.widget_ID].version;
 console.log(scriptversion); 
 if (this.version !== scriptversion) {
 Pasteboard.copy(scriptList[this.widget_ID].url);
   Safari.open("scriptable:///run?scriptName=安装小助手");
+  }
 };
 
 const w = new ListWidget();
