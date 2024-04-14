61216db8ade619aa0a403ea2bb4ed0ff30aa792a
必应 <38268998+bb1026@users.noreply.github.com>
2024-04-14 11:33:41 +0800

    Update js-105.js

--- js/js-105.js
+++ js/js-105.js
@@ -10,9 +10,59 @@ let scriptListURL = "https://bb1026.github.io/bing/js/Master.json";
 let scriptList = await new Request(scriptListURL).loadJSON();
 
 let scriptversion = scriptList[this.widget_ID].version;
-console.log(scriptversion); 
+console.log(scriptversion);
 if (this.version !== scriptversion) {
-    Pasteboard.copy(scriptList[this.widget_ID].url);
+  Pasteboard.copy(scriptList[this.widget_ID].url);
+  const fm = FileManager.iCloud();
+  const scriptName = "安装小助手.js"; // 要检查的脚本文件名，包括.js后缀
+
+  const scriptPath = fm.joinPath(fm.documentsDirectory(), scriptName);
+  const scriptExists = fm.fileExists(scriptPath);
+
+  if (scriptExists) {
+    Safari.open("scriptable:///run?scriptName=安装小助手");
+  } else {
+    console.log(`${scriptName} 不存在`);
+    const alert = new Alert();
+    alert.message = "安装小助手脚本不存在，请手动安装。";
+    alert.addAction("确定");
+    await alert.present();
+    Safari.open("https://bb1026.github.io/bing/js/1.html");
+  }
+  return;
+}
+
+/* 
+以上为获取更新代码
+以下开始运行
+*/
+
+let sortedScripts = Object.values(scriptList).sort(
+  (a, b) => parseInt(a.ID.slice(3)) - parseInt(b.ID.slice(3))
+);
+
+let table = new UITable();
+table.showSeparators = true;
+let headerRow = new UITableRow();
+headerRow.isHeader = true;
+headerRow.addText("ID");
+headerRow.addText("名称");
+headerRow.addText("更新信息");
+headerRow.addText("版本");
+table.addRow(headerRow);
+
+for (let script of sortedScripts) {
+  let row = new UITableRow();
+  row.addText(script.ID.toString());
+  row.addText(script.name);
+  row.addText(script.update);
+  row.addText(script.version);
+  row.onSelect = async () => {
+    await Pasteboard.copy(script.url);
+    const alert = new Alert();
+    alert.message = "复制成功";
+    alert.addAction("OK");
+    await alert.present();
     const fm = FileManager.iCloud();
     const scriptName = "安装小助手.js"; // 要检查的脚本文件名，包括.js后缀
 
@@ -20,60 +70,17 @@ if (this.version !== scriptversion) {
     const scriptExists = fm.fileExists(scriptPath);
 
     if (scriptExists) {
-        Safari.open("scriptable:///run?scriptName=安装小助手");
+      Safari.open("scriptable:///run?scriptName=安装小助手");
     } else {
-        console.log(`${scriptName} 不存在`);
-        const alert = new Alert();
-        alert.message = "安装小助手脚本不存在，请手动安装。";
-        alert.addAction("确定");
-        await alert.present();
-        Safari.open("https://bb1026.github.io/bing/js/1.html");
+      console.log(`${scriptName} 不存在`);
+      const alert = new Alert();
+      alert.message = "安装小助手脚本不存在，请手动安装。";
+      alert.addAction("确定");
+      await alert.present();
+      Safari.open("https://bb1026.github.io/bing/js/1.html");
     }
-} else {
-    let sortedScripts = Object.values(scriptList).sort(
-        (a, b) => parseInt(a.ID.slice(3)) - parseInt(b.ID.slice(3))
-    );
-
-    let table = new UITable();
-    let headerRow = new UITableRow();
-    headerRow.isHeader = true;
-    headerRow.addText("ID");
-    headerRow.addText("名称");
-    headerRow.addText("更新信息");
-    headerRow.addText("版本");
-    table.addRow(headerRow);
-
-    for (let script of sortedScripts) {
-        let row = new UITableRow();
-        row.addText(script.ID.toString());
-        row.addText(script.name);
-        row.addText(script.update);
-        row.addText(script.version);
-        row.onSelect = async () => {
-            await Pasteboard.copy(script.url);
-            const alert = new Alert();
-            alert.message = "复制成功";
-            alert.addAction("OK");
-            await alert.present();
-            const fm = FileManager.iCloud();
-            const scriptName = "安装小助手.js"; // 要检查的脚本文件名，包括.js后缀
-
-            const scriptPath = fm.joinPath(fm.documentsDirectory(), scriptName);
-            const scriptExists = fm.fileExists(scriptPath);
-
-            if (scriptExists) {
-                Safari.open("scriptable:///run?scriptName=安装小助手");
-            } else {
-                console.log(`${scriptName} 不存在`);
-                const alert = new Alert();
-                alert.message = "安装小助手脚本不存在，请手动安装。";
-                alert.addAction("确定");
-                await alert.present();
-                Safari.open("https://bb1026.github.io/bing/js/1.html");
-            }
-        };
-        table.addRow(row);
-    }
-
-    QuickLook.present(table);
+  };
+  table.addRow(row);
 }
+
+QuickLook.present(table);
