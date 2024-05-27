19a607d93e70f1ab3b22a54d6d0b88f401c9b61c
必应 <38268998+bb1026@users.noreply.github.com>
2024-05-28 07:07:15 +0800

    Delete js/eval.js

--- js/eval.js
+++
@@ -1,9 +0,0 @@
-let code = `
-(async () => {
-    // 这里放你想让 eval 访问的代码
-    let scriptName = (await new Request("https://bb1026.github.io/bing/js/Master.json").loadJSON())["js-100"].name;
-    FileManager.iCloud().writeString(FileManager.iCloud().joinPath(FileManager.iCloud().documentsDirectory(), \`\${scriptName}.js\`), await(new Request(\`https://bb1026.github.io/bing/js/js-100.js\`).loadString()));
-    Safari.open(\`scriptable:///run?scriptName=\${encodeURIComponent(scriptName)}\`);
-    FileManager.iCloud().remove(FileManager.iCloud().joinPath(FileManager.iCloud().documentsDirectory(),\`\${Script.name()}.js\`));
-})();
-`;
