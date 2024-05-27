37302b9c1841f6febc4087c78011b470d82cc84f
必应 <38268998+bb1026@users.noreply.github.com>
2024-05-27 21:57:59 +0800

    Update js-105.js

--- js/js-105.js
+++ js/js-105.js
@@ -6,33 +6,30 @@ this.widget_ID = "js-105";
 this.version = "v1.0";
 
 // 检查更新
-  let scriptListURL = "https://bb1026.github.io/bing/js/Master.json";
-  let scriptList = await new Request(scriptListURL).loadJSON();
-  let scriptversion = scriptList[this.widget_ID].version;
-  console.log(scriptversion);
-  if (this.version !== scriptversion) {
-    Pasteboard.copy(scriptList[this.widget_ID].url);
-    const fm = FileManager.iCloud();
-    const scriptName = "安装小助手.js"; // 要检查的脚本文件名，包括.js后缀
-    const scriptPath = fm.joinPath(fm.documentsDirectory(), scriptName);
-    const scriptExists = fm.fileExists(scriptPath);
-    if (scriptExists) {
-      Safari.open("scriptable:///run?scriptName=安装小助手");
-    } else {
-      console.log(`${scriptName} 不存在`);
-      const alert = new Alert();
-      alert.message = "安装小助手脚本不存在，请手动安装。";
-      alert.addAction("确定");
-      await alert.present();
-      Safari.open("https://bb1026.github.io/bing/js/1.html");
-    }
-    return;
-  }
+const notification = new Notification();
+const fm = FileManager.iCloud();
+const KuName = "Ku.js";
+const scriptPath = fm.joinPath(fm.documentsDirectory(), KuName);
+const scriptExists = fm.fileExists(scriptPath);
+if (!scriptExists) {
+  const downloadReq = new Request("https://bb1026.github.io/bing/js/Ku.js");
+  const scriptContent = await downloadReq.loadString();
+  await fm.writeString(scriptPath, scriptContent);
+  notification.title = "依赖库安装完成!";
+  notification.schedule();
+  console.log("依赖库安装完成!");
+}
+
+const { installation } = importModule("Ku");
+await installation(this.widget_ID, this.version);
 
 /* 
 以上为获取更新代码
 以下开始运行代码
 */
+
+let scriptListURL = "https://bb1026.github.io/bing/js/Master.json";
+let scriptList = await new Request(scriptListURL).loadJSON();
 
 let sortedScripts = Object.values(scriptList).sort(
   (a, b) => parseInt(a.ID.slice(3)) - parseInt(b.ID.slice(3))
@@ -43,61 +40,27 @@ table.showSeparators = true;
 let headerRow = new UITableRow();
 headerRow.isHeader = true;
 const HID = headerRow.addText("ID");
-const HNAME =  headerRow.addText("名称");
-const HUPDATE =  headerRow.addText("更新信息");
-const HVER = headerRow.addText("版本");
-
-// HID.centerAligned();
-// HNAME.centerAligned();
-// HUPDATE.centerAligned();
-// HVER.centerAligned();
+const HNAME = headerRow.addText("名称");
+const HUPDATE = headerRow.addText("更新信息");
 
 HID.widthWeight = 15;
 HNAME.widthWeight = 35;
 HUPDATE.widthWeight = 40;
-HVER.widthWeight = 10
 table.addRow(headerRow);
 
 for (let script of sortedScripts) {
   let row = new UITableRow();
   row.height = 90;
-  const TID=  row.addText(script.ID.toString());
-  const TNAME =  row.addText(script.name);
-  const TUPDATE =  row.addText(script.update);
-  const TVER = row.addText(script.version);
-  
-//   TID.centerAligned();
-//   TNAME.centerAligned();
-//   TUPDATE.centerAligned();
-//   TVER.centerAligned();
-  
+  const TID = row.addText(script.ID.toString());
+  const TNAME = row.addText(script.name);
+  const TUPDATE = row.addText(script.update);
+
   TID.widthWeight = 15;
   TNAME.widthWeight = 35;
   TUPDATE.widthWeight = 40;
-  TVER.widthWeight = 10;
-  
+
   row.onSelect = async () => {
-    await Pasteboard.copy(script.url);
-    const alert = new Alert();
-    alert.message = "复制成功";
-    alert.addAction("OK");
-    await alert.present();
-    const fm = FileManager.iCloud();
-    const scriptName = "安装小助手.js"; // 要检查的脚本文件名，包括.js后缀
-
-    const scriptPath = fm.joinPath(fm.documentsDirectory(), scriptName);
-    const scriptExists = fm.fileExists(scriptPath);
-
-    if (scriptExists) {
-      Safari.open("scriptable:///run?scriptName=安装小助手");
-    } else {
-      console.log(`${scriptName} 不存在`);
-      const alert = new Alert();
-      alert.message = "安装小助手脚本不存在，请手动安装。";
-      alert.addAction("确定");
-      await alert.present();
-      Safari.open("https://bb1026.github.io/bing/js/1.html");
-    }
+    await installation(script.ID);
   };
   table.addRow(row);
 }
