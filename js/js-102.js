a737d22742cd3f495848262f5c3b327f7ee1aaca
必应 <38268998+bb1026@users.noreply.github.com>
2025-04-07 21:57:10 +0800

    更新 js-102.js

--- js/js-102.js
+++ js/js-102.js
@@ -201,7 +201,7 @@ async function createAccessoryCircular() {
 
 async function CheckKu() {
   const notification = new Notification();
-  const fm = FileManager.iCloud();
+  const fm = FileManager.local();
   const KuName = "Ku.js";
   const scriptPath = fm.joinPath(fm.documentsDirectory(), KuName);
   const scriptExists = fm.fileExists(scriptPath);
