40d98759e598969510a0f64302406e2e1cc4b98b
必应 <38268998+bb1026@users.noreply.github.com>
2025-04-08 06:22:08 +0800

    更新 js-107.js

--- js/js-107.js
+++ js/js-107.js
@@ -1,6 +1,7 @@
-(async () => {// Variables used by Scriptable.
+// Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: bus;
+(async () => {
 this.name = "附近的Bus SG";
 this.widget_ID = "js-107";
 this.version = "v1.5";
