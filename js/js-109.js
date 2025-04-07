947452236f0d1b97f68a2818f9ddfbc458a48731
必应 <38268998+bb1026@users.noreply.github.com>
2025-04-08 06:22:28 +0800

    更新 js-109.js

--- js/js-109.js
+++ js/js-109.js
@@ -1,6 +1,7 @@
-(async () => {// Variables used by Scriptable.
+// Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-green; icon-glyph: bus-alt;
+(async () => {
 this.name = "BusGo";
 this.widget_ID = "js-109";
 this.version = "v2.0";
