18234d4277068eb9b47b3aa191922003285a35fd
必应 <38268998+bb1026@users.noreply.github.com>
2025-04-07 21:58:55 +0800

    更新 js-106.js

--- js/js-106.js
+++ js/js-106.js
@@ -80,7 +80,7 @@ for (let item of musicData) {
 
 async function CheckKu() {
   const notification = new Notification();
-  const fm = FileManager.iCloud();
+  const fm = FileManager.local();
   const KuName = "Ku.js";
   const scriptPath = fm.joinPath(fm.documentsDirectory(), KuName);
   const scriptExists = fm.fileExists(scriptPath);
