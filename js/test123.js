ada8ed5a08197ef7fb850b6e9b8f345fea73cf0b
必应 <38268998+bb1026@users.noreply.github.com>
2025-04-08 11:30:46 +0800

    Create test123.js

---
+++ js/test123.js
@@ -0,0 +1,12 @@
+{
+  "script" : "test123456\nabcdeABCDE",
+  "icon" : {
+    "color" : "red",
+    "glyph" : "bicycle"
+  },
+  "share_sheet_inputs" : [
+
+  ],
+  "name" : "test",
+  "always_run_in_app" : false
+}
