15079de7dc6813a9ae73b25884c149f90a60eb4c
必应 <38268998+bb1026@users.noreply.github.com>
2025-04-17 07:49:31 +0800

    更新 js-103.js

--- js/js-103.js
+++ js/js-103.js
@@ -633,7 +633,7 @@ async function CheckKu() {
   }
 
 ({ installation } = importModule("Ku"));
-  if (typeof Ku.installation !== "function") throw new Error("数据库模块无效");
+  if (typeof installation !== "function") throw new Error("数据库模块无效");
 }
 
 // 运行函数
