e11b443f27e5ba33df9e835ad5e4c223fe9efd56
必应 <38268998+bb1026@users.noreply.github.com>
2025-04-30 19:38:21 +0800

    更新 js-101.js

--- js/js-101.js
+++ js/js-101.js
@@ -75,6 +75,13 @@ async function CheckKu() {
   let needDownload = false;
 
   try {
+    ({
+      installation,
+      getUrls,
+      generateScriptsHTML,
+      createHTMLContent
+    } = importModule("Ku"));
+    
     if (typeof installation !== "function") {
       console.log("数据库模块无效，准备重新下载");
       needDownload = true;
