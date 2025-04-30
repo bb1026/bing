7b265c24d6aae20872f382aeee8fadc1388beec1
必应 <38268998+bb1026@users.noreply.github.com>
2025-04-30 19:33:46 +0800

    更新 js-105.js

--- js/js-105.js
+++ js/js-105.js
@@ -134,12 +134,19 @@ async function CheckKu() {
   let needDownload = false;
 
   try {
-    if (!fm.fileExists(path) || !fm.readString(path).includes("installation")) {
-      console.log("数据库异常，准备重新下载");
+    ({
+      installation,
+      getUrls,
+      generateScriptsHTML,
+      createHTMLContent
+    } = importModule("Ku"));
+    
+    if (typeof installation !== "function") {
+      console.log("数据库模块无效，准备重新下载");
       needDownload = true;
     }
   } catch {
-    console.log("数据库异常，准备重新下载");
+    console.log("数据库导入失败，准备重新下载");
     needDownload = true;
   }
 
@@ -147,13 +154,13 @@ async function CheckKu() {
     fm.writeString(path, await new Request(url).loadString());
     if (fm.isFileStoredIniCloud(path)) await fm.downloadFileFromiCloud(path);
     console.log("数据库下载完成");
+    
+    ({
+      installation,
+      getUrls,
+      generateScriptsHTML,
+      createHTMLContent
+    } = importModule("Ku"));
+    if (typeof installation !== "function") throw new Error("数据库模块无效");
   }
-
-  ({
-    installation,
-    getUrls,
-    generateScriptsHTML,
-    createHTMLContent
-  } = importModule("Ku"));
-  if (typeof installation !== "function") throw new Error("数据库模块无效");
 }
