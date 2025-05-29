03abb8db68a6f2e09202a16d5747b5c24e243f43
必应 <38268998+bb1026@users.noreply.github.com>
2025-05-29 20:34:42 +0800

    更新 Ku.js

--- js/Ku.js
+++ js/Ku.js
@@ -3,7 +3,7 @@
 // icon-color: green; icon-glyph: vector-square;
 this.name = "Ku";
 this.widget_ID = "js-999";
-this.version = "v3.93";
+this.version = "v3.94";
 
 function getUrls() {
   const BASE_URL = "https://raw.githubusercontent.com/bb1026/bing/main/"
