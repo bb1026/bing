0b8c9aceb26b5e672d2695d09f6610217b15cc8b
必应 <38268998+bb1026@users.noreply.github.com>
2025-06-11 20:11:05 +0800

    更新 js-102.js

--- js/js-102.js
+++ js/js-102.js
@@ -237,7 +237,7 @@ if (config.runsInAccessoryWidget) {
 async function CheckKu() {
   const fm = FileManager.local();
   const path = fm.joinPath(fm.documentsDirectory(), "Ku.js");
-  const url = "https://raw.githubusercontent.com/bb1026/bing/main/js/Ku.js";
+  const url = "https://bing.0515364.xyz/js/Ku.js";
   let needDownload = false;
 
   try {
