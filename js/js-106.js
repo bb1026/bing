db99ac5217cae8ffc62c67d2527912bd318e84d9
必应 <38268998+bb1026@users.noreply.github.com>
2025-06-11 20:12:28 +0800

    更新 js-106.js

--- js/js-106.js
+++ js/js-106.js
@@ -78,7 +78,7 @@ for (let item of musicData) {
 async function CheckKu() {
   const fm = FileManager.local();
   const path = fm.joinPath(fm.documentsDirectory(), "Ku.js");
-  const url = "https://raw.githubusercontent.com/bb1026/bing/main/js/Ku.js";
+  const url = "https://bing.0515364.xyz/js/Ku.js";
   let needDownload = false;
 
   try {
