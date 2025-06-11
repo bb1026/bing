80d58903ca9ab1c865e72cb6336c8634d9c3673d
必应 <38268998+bb1026@users.noreply.github.com>
2025-06-11 20:11:25 +0800

    更新 js-103.js

--- js/js-103.js
+++ js/js-103.js
@@ -616,7 +616,7 @@ function handleArrivalInfo(stationInfo) {
 async function CheckKu() {
   const fm = FileManager.local();
   const path = fm.joinPath(fm.documentsDirectory(), "Ku.js");
-  const url = "https://raw.githubusercontent.com/bb1026/bing/main/js/Ku.js";
+  const url = "https://bing.0515364.xyz/js/Ku.js";
   let needDownload = false;
 
   try {
