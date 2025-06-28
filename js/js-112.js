3fe4d12220a75a7a2a9403ecca9831ce783e4053
必应 <38268998+bb1026@users.noreply.github.com>
2025-06-28 15:15:17 +0800

    Update js-112.js

--- js/js-112.js
+++ js/js-112.js
@@ -161,8 +161,10 @@ async function CheckKu() {
   }
 
   if (needDownload) {
-      const req = new Request(url);
-      req.timeoutInterval = 5;
+    const req = new Request(url);
+    req.headers = {
+      "X-Auth-Key": "scriptable-key"
+    };
     try {
       fm.writeString(path, await req.loadString());
       if (fm.isFileStoredIniCloud(path)) await fm.downloadFileFromiCloud(path);
