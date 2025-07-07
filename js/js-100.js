7a8004d8d562b63d225018f1f14001742ff08da0
必应 <38268998+bb1026@users.noreply.github.com>
2025-07-07 12:05:16 +0800

    更新 js-100.js

--- js/js-100.js
+++ js/js-100.js
@@ -1,6 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: green; icon-glyph: hippo;
+// icon-color: green; icon-glyph: hands-helping;
 this.name = "安装小助手";
 this.widget_ID = "js-100";
 this.version = "v1.4";
